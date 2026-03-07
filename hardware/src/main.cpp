ec28606838fdcfaf850b756bb5c688915f50f531
Ben Gilbert <bensgilbert@outlook.com>
2026-03-07 17:22:46 +0000

    Add hardwaore dependecues (JSON, WebSocket)

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,12 +1,41 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
+#include <WebSocketsClient.h>
 #include <WiFi.h>
 
 const char *ssid = "SSID";
 const char *password = "PASSWORD";
 
-void setup()
+WebSocketsClient webSocket;
+
+void webSocketEventHandler(WStype_t type, uint8_t *payload, size_t length)
 {
-    WiFi.begin(ssid, password);
+    switch (type)
+    {
+    case WStype_CONNECTED:
+        break;
+
+    case WStype_TEXT:
+        break;
+
+    case WStype_DISCONNECTED:
+        break;
+    }
 }
 
-void loop() {}
+void setup()
+{
+    Serial.begin(115200);
+
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+        delay(500);
+
+    webSocket.begin("192.168.1.100", 5000, "/hardware");
+    webSocket.onEvent(webSocketEventHandler);
+}
+
+void loop()
+{
+    webSocket.loop();
+}
