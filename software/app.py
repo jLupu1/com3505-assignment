87fc28abdf16d74947ce4f0fa3847dfa8a07c19e
Ben Gilbert <bensgilbert@outlook.com>
2026-03-07 04:03:15 +0000

    Flask WebSocket proxy server

--- software/app.py
+++ software/app.py
@@ -1,12 +1,49 @@
-from flask import Flask, send_file
+from flask import Flask, request, send_file
+from simple_websocket import ConnectionClosed, Server
 
 app = Flask(__name__)
+
+hardware_ws = None
+software_ws = None
 
 
 @app.route("/")
 def index():
-    # TODO render_template?
     return send_file("index.html")
+
+
+@app.route("/hardware", websocket=True)
+def hardware():
+    global hardware_ws, software_ws
+
+    ws = Server.accept(request.environ)
+    hardware_ws = ws
+
+    try:
+        while True:
+            message = ws.receive()
+
+            if software_ws:
+                software_ws.send(message)
+    except ConnectionClosed:
+        hardware_ws = None
+
+
+@app.route("/software", websocket=True)
+def software():
+    global hardware_ws, software_ws
+
+    ws = Server.accept(request.environ)
+    software_ws = ws
+
+    try:
+        while True:
+            message = ws.receive()
+
+            if hardware_ws:
+                hardware_ws.send(message)
+    except ConnectionClosed:
+        software_ws = None
 
 
 if __name__ == "__main__":
